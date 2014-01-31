e6a7bc6548f3a2417c40a497e479c477f9f4891e
roroah <kenshirooni@gmail.com>
2014-01-31 08:49:43 +0100

    Added materials definition for animTexture. Added code to testCursor in MapEditor, can be moved outside if needed. Added a new plane from ShaderblowSource since we got an issue with our plane. @todo see why we got that issue Added some texture, all who are in the forder ToRemove got i'll say no licence so we can't use them, it's used for testing purpose on different kind of spriteSheet. There still some issue but the cursor is working :D Renamed some files. The issue with the hexGap could now be resolved there is a comment added who explain why (i guess) it do that but since it's working pretty nicely nothing have been tested to resolve the issue. Fixed import.

--- MultiverseKing/src/test/MapEditorTest.java
+++ MultiverseKing/src/test/MapEditorTest.java
@@ -13,7 +13,6 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
-import com.jme3.math.Matrix3f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;

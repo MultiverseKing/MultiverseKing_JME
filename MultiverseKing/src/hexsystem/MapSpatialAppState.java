e724f74c2088bf9f2eddba53160a2a11799e2736
roroah <kenshirooni@gmail.com>
2014-02-07 08:31:52 +0100

    MeshManagerV2 : Handle greedy meshing and flat terrain, minor change have to be done to the texture to work with the new behaviour.

--- MultiverseKing/src/hexsystem/MapSpatialAppState.java
+++ MultiverseKing/src/hexsystem/MapSpatialAppState.java
@@ -28,7 +28,7 @@ public class MapSpatialAppState extends AbstractAppState implements TileChangeLi
     private MapData mapData;
     private Geometry[][] tiles;
     Material[] materials;
-    MeshManager meshManager;
+    MeshManagerV2 meshManager;
     /**
      * To got a gap between Hex Change tileSize during initialize and set it to 1.
      */
@@ -48,34 +48,40 @@ public class MapSpatialAppState extends AbstractAppState implements TileChangeLi
 
 
         //TODO: Create mesh in a nicer way
-        meshManager = new MeshManager();
-        tileSize = meshManager.getHexWidth()/2;
-        Mesh hexagon = meshManager.getTile();
-        for (int x = 0; x < tileData.length; x++) {
-            for (int y = 0; y < tileData.length; y++) {
-//TODO: Take tileheight into account
-                Geometry geom = new Geometry(x + "|" + y, hexagon);
-                //TODO: Set Position and scale correctly
-//                geom.setLocalScale(tileSize);
-
-                geom.setLocalTranslation(getSpatialPositionForTile(x, y));
-
-                geom.setMaterial(getMaterialForTile(tileData[x][y]));
-                tiles[x][y] = geom;
-                mapNode.attachChild(geom);
-            }
-        }
+        meshManager = new MeshManagerV2();
+//        tileSize = meshManager.getHexWidth()/2;
+//        Mesh hexagon = meshManager.getTile();
+//        for (int x = 0; x < tileData.length; x++) {
+//            for (int y = 0; y < tileData.length; y++) {
+////TODO: Take tileheight into account
+//                Geometry geom = new Geometry(x + "|" + y, hexagon);
+//                //TODO: Set Position and scale correctly
+////                geom.setLocalScale(tileSize);
+//
+//                geom.setLocalTranslation(getSpatialPositionForTile(x, y));
+//
+//                geom.setMaterial(getMaterialForTile(tileData[x][y]));
+//                tiles[x][y] = geom;
+//                mapNode.attachChild(geom);
+//            }
+//        }
+        Mesh hexagon = meshManager.generateChunk(new Vector2Int(2, 2));
+        hexagon.setPointSize(4.0f);
+//        hexagon.setMode(Mesh.Mode.Points);
+        Geometry geom = new Geometry(25 + "|" + 25, hexagon);
+        geom.setMaterial(materials[0]);
+        mapNode.attachChild(geom);
     }
 
     private void setupMaterials(AssetManager assetManager) {
         materials = new Material[ElementalAttribut.getSize()];
         int i = 0;
         for (ElementalAttribut e : ElementalAttribut.values()) { //ElementalAttribut.SIZE do the same
-            Material mat = assetManager.loadMaterial("Materials/hexMat.j3m");
+            Material mat = assetManager.loadMaterial("Materials/debugMat.j3m");
             Texture2D tex = (Texture2D) assetManager.loadTexture("Textures/Test/" + e.name() + "Center.png");
             tex.setWrap(Texture.WrapMode.Repeat);
-//            g.getMaterial().getParam("Diffuse").setValue(tex);
-//            mat.getAdditionalRenderState().setWireframe(true); //needed for debug on MeshManager
+////            g.getMaterial().getParam("Diffuse").setValue(tex);
+////            mat.getAdditionalRenderState().setWireframe(true); //needed for debug on MeshManager
             mat.setTexture("ColorMap", tex);
             materials[i++] = mat;
         }
@@ -89,8 +95,8 @@ public class MapSpatialAppState extends AbstractAppState implements TileChangeLi
         HexTile oldTile = event.getOldTile();
         HexTile newTile = event.getNewTile();
         if (newTile.getHexElement() != oldTile.getHexElement()) {
-            tiles[event.getX()][event.getY()].setMaterial(getMaterialForTile(event.getNewTile()));
-            tiles[event.getX()][event.getY()].setMesh(meshManager.getHeightedTile(event.getNewTile().getHeight()));
+//            tiles[event.getX()][event.getY()].setMaterial(getMaterialForTile(event.getNewTile()));
+//            tiles[event.getX()][event.getY()].setMesh(meshManager.getHeightedTile(event.getNewTile().getHeight()));
         } else if (newTile.getHeight() != oldTile.getHeight()) {
             //TODO: Regenerate mesh
             System.out.println("Work");

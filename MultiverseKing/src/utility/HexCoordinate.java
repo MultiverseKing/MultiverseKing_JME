bb00ab96ae253336cc77d12cc2cec1543558100c
eike <eike@eikelap>
2014-03-05 13:42:50 +0100

    new HexCoordinate

--- MultiverseKing/src/utility/HexCoordinate.java
+++ MultiverseKing/src/utility/HexCoordinate.java
@@ -4,158 +4,91 @@
  */
 package utility;
 
-import com.jme3.math.Vector2f;
-
 /**
  *
  * @author Roah with the help of : ArtemisArt => http://artemis.art.free.fr/ &&
- * http://www.redblobgames.com
- *
- * This Class is only used as a converter system so we can simplifie algorithm.
+ * http://www.redblobgames.com --Changed version by toboi-- This Class is only
+ * used as a converter system so we can simplifie algorithm.
  */
 public final class HexCoordinate {
 
-    public final class Cube {
+    public static final int TYPE_OFFSET = 0;
+    public static final int TYPE_AXIAL = 1;
+    public static final int TYPE_CUBIC = 2;
+    /**
+     * Axial position in Grid. q == x
+     */
+    public int q;
+    /**
+     * Axial position in Grid. r == z (or Y)
+     */
+    public int r;
 
-        public final int x;
-        public final int y;
-        public final int z;
-
-        /**
-         * Create new Cube coordinate.
-         */
-        public Cube(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-        
-        /**
-         * Convert Cube coordinate to Axial coordinate.
-         * @return Axial coordinate.
-         */
-        public Axial toAxial() {
-            return new Axial(x, z);
-        }
-        
-        /**
-         * Convert Cube coordinate to Odd-R Offset coordinate.
-         * @return Odd-R Offset coordinate.
-         */
-        public Offset toOffset() {
-            return new Offset(x + (z - (z & 1)) / 2, z);
-        }
-
-        @Override
-        public String toString() {
-            return x+"|"+y+"|"+z;
-        }
-        
+    private static Vector2Int offsetToAxial(Vector2Int data) {
+        return new Vector2Int(data.x - (data.y - (data.y & 1)) / 2, data.y);
     }
 
-    public final class Offset {
-
-
-        /**
-         * Offset position in odd-r Grid. Currently generated Grid. q == x
-         */
-        public final int q;
-        /**
-         * Offset position in Odd-R Grid. Currently generated Grid. r == z (or Y)
-         */
-        public final int r;
-
-        /**
-         * Create new Odd-R Offset grid position, Currently generated Grid.
-         * @param q = x position of the tile in hexMap in Offset coordinate.
-         * @param r = z (or y) position of the tile in hexMap in Offset coordinate.
-         */
-        public Offset(int q, int r) {
-            this.q = q;
-            this.r = r;
-        }
-
-        public Offset(Vector2Int value) {
-            this(value.x, value.y);
-        }
-
-        /**
-         * Convert Odd-R Offset to Cube coordinate.
-         * @return Cube coordinate.
-         */
-        public Cube toCube() {
-            return new Cube(q - (r - (r & 1)) / 2, -(q - (r - (r & 1)) / 2) - r, r);
-        }
-
-        /**
-         * Convert Odd-R Offset to Axial coordinate.
-         * @return Axial coordinate.
-         */
-        public Axial toAxial() {
-            return new Axial(q - (r - (r & 1)) / 2, r);
-        }
-        
-        @Override
-        public String toString() {
-            return q+"|"+r;
-        }
-
-        public Vector2Int toVector2Int() {
-            return new Vector2Int(this.q, this.r);
-        }
-
-        public boolean equalsCoord(Offset tilePos) {
-            if(tilePos.q == this.q && tilePos.r == this.r){
-                return true;
-            } else {
-                return false;
-            }
-        }
+    private static Vector2Int cubicToAxial(Vector3Int data) {
+        return new Vector2Int(data.x, data.z);
     }
 
-    public final class Axial {
+    public Vector3Int getAsCubic() {
+        return new Vector3Int(q, -q - r, r);
+    }
 
-        /**
-         * Axial position in Grid. q == x
-         */
-        public final int q;
-        /**
-         * Axial position in Grid. r == z (or Y)
-         */
-        public final int r;
+    public Vector2Int getAsOffset() {
+        return new Vector2Int(q + (r - (r & 1)) / 2, r);
+    }
 
-        /**
-         * Create new Axial coordinate.
-         * @param q x position of the tile in hexMap in Axial coordinate.
-         * @param r z position of the tile in hexMap in Axial coordinate.
-         */
-        public Axial(int q, int r) {
-            this.q = q;
-            this.r = r;
-        }
+    public Vector2Int getAsAxial() {
+        return new Vector2Int(q, r);
+    }
 
-        /**
-         * Convert Axial coordinate to Cube coordinate.
-         * @return Axial coordinate.
-         */
-        public Cube toCube() {
-            return new Cube(q, -q - r, r);
-        }
+    /**
+     * Only for internal use
+     *
+     * @param q
+     * @param r
+     */
+    private HexCoordinate(int q, int r) {
+        this.q = q;
+        this.r = r;
+    }
 
-        /**
-         * Convert Axial coordinate to Odd-R Offset coordinate.
-         * @return Odd-R Offset coordinate.
-         */
-        public Offset toOffset() {
-            return new Offset(q + (r - (r & 1)) / 2, r);
+    public HexCoordinate(int type, Vector2Int pos) {
+        if (type == TYPE_OFFSET) {
+            pos = offsetToAxial(pos);
         }
-        
-        /**
-         * @return faf
-         */
-        @Override
-        public String toString() {
-            return q+"|"+r;
+        q = pos.x;
+        r = pos.y;
+    }
+
+    public HexCoordinate(int type, Vector3Int pos) {
+        if (type != TYPE_CUBIC) {
+            throw new UnsupportedOperationException("Only TYPE_CUBIC expects a Vector3Int!");
         }
+        Vector2Int posAxial = cubicToAxial(pos);
+        q = posAxial.x;
+        r = posAxial.y;
+    }
+
+    /**
+     * @return faf
+     */
+    @Override
+    public String toString() {
+        return q + "|" + r;
+    }
+
+    public HexCoordinate[] getNeighbours() {
+        HexCoordinate[] neighbours = new HexCoordinate[]{
+            new HexCoordinate(q + 1, r),
+            new HexCoordinate(q + 1, r - 1),
+            new HexCoordinate(q, r - 1),
+            new HexCoordinate(q - 1, r),
+            new HexCoordinate(q - 1, r + 1),
+            new HexCoordinate(q, r + 1)
+        };
+        return neighbours;
     }
 }
--- MultiverseKing/src/utility/Vector2Int.java
+++ MultiverseKing/src/utility/Vector2Int.java
@@ -5,45 +5,40 @@
 package utility;
 
 import com.jme3.math.Vector2f;
-import utility.HexCoordinate.Offset;
 
 /**
- * I don't like to use 2 int or Vector2f for some variable, fully subjective thing nothing more.
+ * I don't like to use 2 int or Vector2f for some variable, fully subjective
+ * thing nothing more.
+ *
  * @author roah
- * @todo implement equals && hashCode Override 
+ * @todo implement equals && hashCode Override
  */
 public class Vector2Int {
-    public static final Vector2Int ZERO = new Vector2Int(0,0);
+
+    public static final Vector2Int ZERO = new Vector2Int(0, 0);
     public int x;
     public int y;
 
     /**
      * Convert the vector2Int to string, formated as : x|y.
+     *
      * @return string "x|y".
      */
     @Override
     public String toString() {
-        return Integer.toString(this.x)+"|"+Integer.toString(this.y);
-    }
-    
-    public Vector2Int(){
-        this(0,0);
+        return Integer.toString(this.x) + "|" + Integer.toString(this.y);
     }
 
-    public Vector2Int(Vector2f value){
-        this((int)value.x, (int)value.y);
+    public Vector2Int() {
+        this(0, 0);
     }
 
-    public Vector2Int(int x, int y){
+    public Vector2Int(Vector2f value) {
+        this((int) value.x, (int) value.y);
+    }
+
+    public Vector2Int(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public boolean equalsCoord(Offset tilePos) {
-        if(tilePos.q == this.x && tilePos.r == this.y){
-            return true;
-        } else {
-            return false;
-        }
     }
 }

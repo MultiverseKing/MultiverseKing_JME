1427147fca324a0aba47ad04a51a73fe3a2f176b
roah <kob-boudha@hotmail.fr>
2015-03-26 11:05:39 +0100

    Remove duplicated directory

--- HexGrid_Editor/src/gui/deprecated/listener/DialogWindowListener.java
+++
@@ -1,9 +0,0 @@
-package gui.deprecated.listener;
-
-/**
- *
- * @author roah
- */
-public interface DialogWindowListener {
-    public void onDialogTrigger(String dialogUID, boolean confirmOrCancel);
-}

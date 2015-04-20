f27ffc3f670ada6dca492f9b252d0a52097b1a86
roah <roah@Roah-Unix-System>
2015-04-20 22:32:13 +0200

    MultiverseKingCore v.2.0.0_pre-Alpha
    - Moving class to HexGridAPI
    - Some compatibility fix (WIP)

---
+++ MultiverseKingGame/src/kingofmultiverse/KingOfMultiverseMain.java
@@ -0,0 +1,16 @@
+package kingofmultiverse;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ *
+ * @author roah
+ */
+public class KingOfMultiverseMain extends SimpleApplication {
+
+    @Override
+    public void simpleInitApp() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

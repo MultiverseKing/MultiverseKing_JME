6ccf7d3681e72d43e4c4869c3de640c59110d725
roah <roah@Roah-Unix-System>
2015-06-17 07:04:05 +0200

    MultiverseKingEditor_v.8.0.0.preAlpha
    - some classpath/folder to fix fome build issue.

---
+++ MultiverseKingEditor/src/org/multiverseking/editor/core/JPlayEditorMenu.java
@@ -0,0 +1,94 @@
+package org.multiverseking.editor.core;
+
+import org.hexgridapi.editor.core.HexGridEditorMain;
+import java.awt.event.ActionEvent;
+import java.util.concurrent.Callable;
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+import javax.swing.border.BevelBorder;
+import org.multiverseking.debug.DebugSystemState;
+import org.multiverseking.debug.RenderDebugSystem;
+import org.multiverseking.field.exploration.ExplorationSystem;
+
+/**
+ *
+ * @author roah
+ */
+public class JPlayEditorMenu extends JMenu {
+
+    private final HexGridEditorMain main;
+    private JButton stopBtn;
+
+    public JPlayEditorMenu(final HexGridEditorMain main) {
+        super("Play");
+        this.main = main;
+        stopBtn = new JButton(new AbstractAction(" Stop") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                main.enqueue(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        stopExploration();
+                        return null;
+                    }
+                });
+            }
+        });
+        stopBtn.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+
+        add(new AbstractAction("Play Exploration") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onAction(e);
+            }
+        });
+        add(new AbstractAction("Play Battle") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onAction(e);
+            }
+        });
+    }
+
+    public void onAction(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Play Battle":
+                JOptionPane.showMessageDialog(main.getRootFrame(), "TODO...");
+                break;
+            case "Play Exploration":
+                main.enqueue(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        if (main.getStateManager().getState(ExplorationSystem.class) != null) {
+                            int result = JOptionPane.showConfirmDialog(main.getRootFrame(), "Exploration mode already running, "
+                                    + "want to shut it off ?", "Exploration", JOptionPane.OK_CANCEL_OPTION);
+                            if (result == 0) {
+                                stopExploration();
+                            }
+                        } else if (main.getStateManager().getState(DebugSystemState.class).getStartPosition() != null) {
+                            main.getStateManager().attach(new ExplorationSystem());
+                            main.getStateManager().getState(RenderDebugSystem.class).setEnabled(false);
+                            main.getRootFrame().getJMenuBar().add(stopBtn);
+                            main.getRootFrame().revalidate();
+                            main.getRootFrame().repaint();
+                        } else {
+                            JOptionPane.showMessageDialog(main.getRootFrame(), "A start position need to be set.");
+                        }
+                        return null;
+                    }
+                });
+                break;
+        }
+    }
+
+    private void stopExploration() {
+        main.getStateManager().detach(main.getStateManager().getState(ExplorationSystem.class));
+        main.getStateManager().getState(RenderDebugSystem.class).setEnabled(true);
+        main.getRootFrame().getJMenuBar().remove(stopBtn);
+        main.getRootFrame().revalidate();
+        main.getRootFrame().repaint();
+    }
+}
---
+++ MultiverseKingEditor/src/org/multiverseking/editor/core/MultiverseEditor.java
@@ -0,0 +1,51 @@
+package org.multiverseking.editor.core;
+
+import org.hexgridapi.editor.core.HexGridEditorMain;
+import org.multiverseking.editor.core.swingcontrol.JESPropertiesPanel;
+import org.multiverseking.EntityDataAppState;
+import org.multiverseking.MultiverCoreGUI;
+import org.multiverseking.debug.DebugSystemState;
+import org.multiverseking.field.position.HexPositionSystem;
+import org.multiverseking.debug.RenderDebugSystem;
+import org.multiverseking.render.RenderSystem;
+import tonegod.gui.core.Screen;
+
+/**
+ *
+ * @author roah
+ */
+public class MultiverseEditor extends HexGridEditorMain implements MultiverCoreGUI {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MultiverseEditor app = new MultiverseEditor("King Of Multiverse Editor");
+            }
+        });
+    }
+
+    public MultiverseEditor(String str) {
+        super(str);
+    }
+    Screen screen;
+
+    @Override
+    public Screen getScreen() {
+        return screen;
+    }
+
+    @Override
+    public void initApplication() {
+        screen = new Screen(this);
+        getStateManager().attachAll(
+                new EntityDataAppState(),
+                new RenderSystem(),
+                new HexPositionSystem(),
+                new DebugSystemState(),
+                new RenderDebugSystem());
+
+        getRootFrame().getJMenuBar().add(new JPlayEditorMenu(this));
+        getHexMapModule().addPropertiesTab(new JESPropertiesPanel(this, getHexMapModule().getMouseSystem()));
+    }
+}
